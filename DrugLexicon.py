35146dddec46525654e3f008139d5b534cf920dc
riggly <ohaganross@gmail.com>
2018-07-08 23:35:49 -0400

    everything in Dr. Percha's test works except the number of number of all terms next steps includes looking at that and seeing what garbage I didn't parse out and then testing all the other functions except get parent since that's not applicable to drugs

--- DrugLexicon.py
+++ DrugLexicon.py
@@ -22,9 +22,6 @@ class DrugLexicon(Lexicon):
                 if name != '':
                     for names in name.split(','):
                         otherNames.append(names.replace('"',''))
-            print(mainName)
-            print(pgkbId)
-            print(otherNames)
             self.addLineDataToMaps(mainName, pgkbId, otherNames, None);
 
 
@@ -47,76 +44,15 @@ class DrugLexicon(Lexicon):
             #need to figure out   addLineDataToMaps(mainName, pgkbId, otherNames, null);
 
 drugs = DrugLexicon()
-drugs.readFromInputStream('drugs.tsv')
+#drugs.readFromInputStream('drugs.tsv')
+drugs.readFromInputStream('drug-lexicon-sample.tsv')
+print(len(drugs.getAllTerms()))
+print(drugs.containsTerm("1,7-dimethylxanthine"))
+print(drugs.containsTerm("paraxanthine"))
+print(drugs.containsTerm("MEDR-640"))
+print(drugs.containsTerm("Adinazolamum [INN-Latin]"))
+print(drugs.containsTerm("T-cell growth factor"))
+print(drugs.containsTerm("Proleukin"))
+print(drugs.containsTerm("Aldose reductase inhibitors"))
+print(drugs.containsTerm(""))
 
-'''
-package lexicon;
-
-// input file is PharmGKB drug lexicon
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
-public class DrugLexicon extends Lexicon {
-    private final Pattern findPattern = Pattern.compile("(?>[^,'\"]++|(['\"])(?>[^\"'\\\\]++|\\\\.|(?!\\1)[\"'])*\\1|(?<=,|^)\\s*(?=,|$))+", Pattern.DOTALL);
-
-    public DrugLexicon(InputStream inputStream) throws IOException {
-        this.readFromInputStream(inputStream);
-    }
-
-    private void processLine(String line) {
-        String[] splitLine = line.split("\t");
-        String pgkbId = splitLine[0];
-        String mainName = splitLine[1];
-        List<String> otherNames = new ArrayList<>();
-        Matcher matcher = findPattern.matcher(splitLine[2].replaceAll("\"\"", "\"")); // other generic names
-        while (matcher.find()) {
-            // eliminate quotation marks from string ends
-            String finalString = matcher.group().replaceAll("^\"|\"$", "");
-            otherNames.add(finalString);
-        }
-        Matcher matcher2 = findPattern.matcher(splitLine[3].replaceAll("\"\"", "\"")); // trade names
-        while (matcher2.find()) {
-            // eliminate quotation marks from string ends
-            String finalString = matcher2.group().replaceAll("^\"|\"$", "");
-            otherNames.add(finalString);
-        }
-        addLineDataToMaps(mainName, pgkbId, otherNames, null);
-    }
-
-    @Override
-    void readFromInputStream(InputStream lexiconInputStream) {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(lexiconInputStream));
-
-        try {
-            // throw away first line because it's file header
-            bufferedReader.readLine();
-            String inputLine = bufferedReader.readLine();
-            while (inputLine != null) {
-                processLine(inputLine);
-                inputLine = bufferedReader.readLine();
-            }
-        } catch (IOException exception) {
-            System.err.println("Cannot parse input stream!");
-        }
-    }
-
-    public static void main(String[] args) throws IOException {
-        String outputFile = args[0];
-
-        DrugLexicon drugLexicon = new DrugLexicon(DrugLexicon.class.getResourceAsStream("/drugs.tsv"));
-
-        PrintWriter printWriter = new PrintWriter(outputFile);
-        drugLexicon.getAllTerms().forEach(s -> printWriter.println(s + "\t" + drugLexicon.getId(s)));
-        printWriter.flush();
-        printWriter.close();
-    }
-}
-'''
